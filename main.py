610ea37ac87d3d4a4af143197450ffb638db4209
xegins <slavakasta@gmail.com>
2020-12-22 15:00:50 +0300

    atomic file writing

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sys
 import json
 
 import typing as tp
+import tempfile as tmp
 
 from hashlib import md5
 from pathlib import Path
+from contextlib import contextmanager
 
 from aiohttp import web
 
@@ -20,6 +22,68 @@ def load_settings(config_path: Path) -> tp.Dict[str, tp.Any]:
         )
     with config_path.open() as config_file:
         return json.load(config_file)
+
+
+@contextmanager
+def tempfile(suffix='', dir: tp.Optional[Path] = None):
+    """ Context for temporary file.
+
+    Will find a free temporary filename upon entering
+    and will try to delete the file on leaving, even in case of an exception.
+
+    Parameters
+    ----------
+    suffix : string
+        optional file suffix
+    dir : Path
+        optional directory to save temporary file in
+    """
+
+    tf = tmp.NamedTemporaryFile(delete=False, suffix=suffix, dir=dir)
+    tf.file.close()
+    try:
+        yield tf.name
+    finally:
+        try:
+            os.remove(tf.name)
+        except OSError as e:
+            if e.errno == 2:
+                pass
+            else:
+                raise
+
+
+@contextmanager
+def open_atomic(filepath: Path, *args, **kwargs):
+    """ Open temporary file object that atomically moves to destination upon
+    exiting.
+
+    Allows reading and writing to and from the same filename.
+
+    The file will not be moved to destination in case of an exception.
+
+    Parameters
+    ----------
+    filepath : Path
+        the file path to be opened
+    fsync : bool
+        whether to force write the file to disk
+    *args : mixed
+        Any valid arguments for :code:`open`
+    **kwargs : mixed
+        Any valid keyword arguments for :code:`open`
+    """
+    fsync = kwargs.get('fsync', False)
+
+    with tempfile(dir=filepath.parent) as tmppath:
+        with open(tmppath, *args, **kwargs) as file:
+            try:
+                yield file
+            finally:
+                if fsync:
+                    file.flush()
+                    os.fsync(file.fileno())
+        Path(tmppath).rename(filepath)
 
 
 @routes.get('/')
@@ -40,11 +104,11 @@ async def upload_file(request: web.Request) -> web.Response:
     body: bytes = await request.read()
     file_hash: str = md5(body).hexdigest()
 
-    file_dir: Path = Path(os.getcwd(), f"store/{file_hash[:2]}/")
+    file_dir: Path = Path(f"store/{file_hash[:2]}/")
     if not file_dir.exists():
         file_dir.mkdir(parents=True)
 
-    with open(Path(file_dir, file_hash), "wb") as f:
+    with open_atomic(file_dir / file_hash, "wb") as f:
         f.write(body)
 
     return web.json_response(
@@ -63,7 +127,7 @@ async def download_file(request: web.Request) -> tp.Union[web.FileResponse, web.
             {
                 "description": f"File {file_hash} - not found!"
             },
-            status=web.HTTPBadRequest.status_code
+            status=web.HTTPNotFound.status_code
         )
     return web.FileResponse(file_path)
 
@@ -98,7 +162,7 @@ async def delete_file(request: web.Request) -> web.Response:
         )
 
     file_path.unlink()
-    # Удаляем директорию, если в ней больше нет файлов
+    # remove dir, if dir is empty
     if not any(file_path.parent.iterdir()):
         file_path.parent.rmdir()
 
